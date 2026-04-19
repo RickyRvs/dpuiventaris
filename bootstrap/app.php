6753889b7f96ee54f7cef3d628579e0b848980f4
RickyRvs <rizkyfadhillah315@gmail.com>
2026-04-19 18:22:12 +0700

    trust proxies for https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,8 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'auth.dpu' => \App\Http\Middleware\AuthMiddleware::class,
         ]);
